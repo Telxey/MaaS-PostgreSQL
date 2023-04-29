1d49c8b8fea462b0ed367e1307ee7bdb250aa3c3
Telxey <131807761+Telxey@users.noreply.github.com>
2023-04-29 13:47:25 -0400

    Create install-DB.sh

---
+++ install-DB.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+wget -qO- https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo tee /etc/apt/trusted.gpg.d/pgdg.asc &>/dev/null
+
+sudo apt update && sudo apt upgrade -y
+sudo apt -y install gnupg2 wget vim bat nano
+ 
+ #2) Install PostgreSQL 15 Database Server and Client
+ 
+sudo apt install postgresql postgresql-client -y
+#sudo -u postgres psql --command '\password'sudo systemctl enable postgresql
+sudo systemctl status postgresql
+sudo systemctl status postgresql
+sudo -u postgres psql -c "SELECT version();"
+sudo -u postgres psql -c "SHOW data_directory;"
+psql --version
+psql -h localhost -U postgres
+rm install-DB.sh
