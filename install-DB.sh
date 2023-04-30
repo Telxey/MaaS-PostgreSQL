ea2d337861e2d528d31b8ad8a1a90585b2d49941
Telxey <131807761+Telxey@users.noreply.github.com>
2023-04-30 00:05:27 -0400

    Update install-DB.sh

--- install-DB.sh
+++ install-DB.sh
@@ -16,5 +16,30 @@ sudo systemctl status postgresql
 sudo -u postgres psql -c "SELECT version();"
 sudo -u postgres psql -c "SHOW data_directory;"
 psql --version
-psql -h localhost -U postgres
-#rm install-DB.sh
+#psql -h localhost -U postgres
+rm install-DB.sh
+
+# Create DB
+# $MAAS_DBUSER = Tabasco4313
+# $MAAS_DBPASS = 4vhjvadnhifchy9p
+# $MAAS_DBNAME = Payback6699
+# $HOSTNAME = pg.f4c1l.com
+
+export MAASDB=maasdb
+export MAASDBUSER=Tabasco4313
+# WARNING you have to escape special characters for the password
+export MAASDBUSERPASSWORD=4vhjvadnhifchy9p
+export HOSTNAME=pg.f4c1l.com
+sudo -u postgres psql -c "CREATE USER \"$MAASDBUSER\" WITH ENCRYPTED PASSWORD '$MAASDBUSERPASSWORD'"
+sudo -u postgres createdb -O $MAASDBUSER $MAASDB
+sudo -u postgres psql --command '\l'
+sudo -u postgres psql --command '\du'
+sudo -u postgres psql --command '\q'
+sudo nano /etc/postgresql/15/main/postgresql.conf
+sudo vi /etc/postgresql/15/main/pg_hba.conf
+
+#add lines 
+# host    maasdb          maasdbuser      172.30.0.0/16           md5
+# host    replication     maasdbrep       172.30.0.0/16           md5
+# host    $MAAS_DBNAME    $MAAS_DBUSER    0/0     md5
+# 
